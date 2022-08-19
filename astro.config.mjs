68acf89084ee49962697da0b9445f6652151de8b
Theo Browne <me@t3.gg>
2022-08-18 22:27:44 -0700

    vercel ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  adapter: vercel(),
+});
