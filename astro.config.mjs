840da840efc0aff393d25b3b97b8fba3ed8afa6d
Theo Browne <me@t3.gg>
2022-08-18 22:31:59 -0700

    server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import vercel from "@astrojs/vercel/edge";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
   adapter: vercel(),
 });
